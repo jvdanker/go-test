08d43815d70cc131e456f930e122b2c45ad211b5
Juan van den Anker <juan@vdanker.net>
2018-06-18 16:59:28 +1200

    Refactoring

--- test.go
+++ test.go
@@ -2,35 +2,24 @@ package main
 
 import (
     "fmt"
-    "io/ioutil"
-     "log"
-     "os"
-     "image"
-     "strings"
-     _ "image/jpeg"
-     "image/png"
-     "math"
-     "image/draw"
+    "os"
+    "math"
+    "image/png"
+    "github.com/jvdanker/go-test/util"
 )
 
-type file struct {
-    name string
-    w int
-    h int
-}
-
 func main() {
-    files := getImages("./images")
+    files := util.GetImages("./images")
 
-    displayImageBounds(files)
+    util.DisplayImageBounds(files)
 
     var itemsPerRow = int(math.Ceil(math.Sqrt(float64(len(files)))))
     fmt.Printf("numberOfItems=%d, itemsPerRow=%d\n", len(files), itemsPerRow)
 
-    maxWidth, maxHeight := calculateMaxWidthAndHeight(files, itemsPerRow)
+    maxWidth, maxHeight := util.CalculateMaxWidthAndHeight(files, itemsPerRow)
     fmt.Printf("maxWidth=%d, maxHeight=%d\n", maxWidth, maxHeight)
 
-    image := mergeImages(files, maxWidth, maxHeight, itemsPerRow)
+    image := util.MergeImages(files, maxWidth, maxHeight, itemsPerRow)
 
     outfilename := "result.png"
     outfile, err := os.Create(outfilename)
@@ -41,125 +30,4 @@ func main() {
     png.Encode(outfile, image)
 }
 
-func getImages(dir string) []file {
-    result := []file{}
 
-    files, err := ioutil.ReadDir(dir)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    for _, f := range files {
-        if strings.HasPrefix(f.Name(), ".") {
-            continue
-        }
-
-        w, h := getImageBounds("images/" + f.Name())
-
-        file := file{ name: f.Name(), w: w, h: h }
-        result = append(result, file)
-    }
-
-    return result
-}
-
-func displayImageBounds(files []file) {
-    for _, f := range files {
-        fmt.Printf("f=%s, w=%d, h=%d\n", f.name, f.w, f.h)
-    }
-}
-
-func mergeImages(files []file, maxWidth, maxHeight, itemsPerRow int) image.Image {
-    var x, y int
-    var curr int
-
-    img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{maxWidth, maxHeight}})
-    maxHeight = 0
-
-    for _, f := range files {
-        if strings.HasPrefix(f.name, ".") {
-            continue
-        }
-
-        src := decodeImage("images/" + f.name)
-        b := src.Bounds()
-
-        if b.Max.Y > maxHeight {
-            maxHeight = b.Max.Y
-        }
-
-        var a = image.Point{x, y}
-
-        draw.Draw(
-            img,
-            image.Rectangle{a, a.Add(image.Point{b.Max.X, b.Max.Y})},
-            src,
-            image.ZP,
-            draw.Src)
-
-        x += b.Max.X
-        curr++
-
-        if curr % itemsPerRow == 0 {
-            x = 0
-            y += maxHeight
-            maxHeight = 0
-        }
-    }
-
-    return img
-}
-
-func calculateMaxWidthAndHeight(files []file, itemsPerRow int) (int, int) {
-    var maxWidth, maxHeight int
-    var curr, currWidth, currHeight int
-
-    for _, f := range files {
-        currWidth += f.w
-
-        if currWidth > maxWidth {
-            maxWidth = currWidth
-        }
-
-        if curr % itemsPerRow == 0 {
-            currWidth = 0
-            currHeight += f.h
-        }
-
-        if currHeight > maxHeight {
-            maxHeight = currHeight
-        }
-
-        curr++
-    }
-
-    return maxWidth, maxHeight
-}
-
-func decodeImage(filename string) image.Image {
-    infile, err := os.Open(filename)
-    if err != nil {
-        // replace this with real error handling
-        panic(err)
-    }
-    defer infile.Close()
-
-    // Decode will figure out what type of image is in the file on its own.
-    // We just have to be sure all the image packages we want are imported.
-    src, _, err := image.Decode(infile)
-    if err != nil {
-        // replace this with real error handling
-        panic(err)
-    }
-
-    return src
-}
-
-func getImageBounds(filename string) (int, int) {
-    src := decodeImage(filename)
-
-    bounds := src.Bounds()
-    w, h := bounds.Max.X, bounds.Max.Y
-
-    return w, h
-}
---
+++ util/util.go
@@ -0,0 +1,143 @@
+package util
+
+import (
+    "fmt"
+    "io/ioutil"
+     "log"
+     "os"
+     "image"
+     "strings"
+     _ "image/jpeg"
+     _ "image/png"
+     "image/draw"
+)
+
+type File struct {
+    Name string
+    W int
+    H int
+}
+
+func GetImages(dir string) []File {
+    result := []File{}
+
+    files, err := ioutil.ReadDir(dir)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, f := range files {
+        if strings.HasPrefix(f.Name(), ".") {
+            continue
+        }
+
+        w, h := GetImageBounds("images/" + f.Name())
+
+        file := File{ Name: f.Name(), W: w, H: h }
+        result = append(result, file)
+    }
+
+    return result
+}
+
+func DisplayImageBounds(files []File) {
+    for _, f := range files {
+        fmt.Printf("f=%s, w=%d, h=%d\n", f.Name, f.W, f.H)
+    }
+}
+
+func DecodeImage(filename string) image.Image {
+    infile, err := os.Open(filename)
+    if err != nil {
+        // replace this with real error handling
+        panic(err)
+    }
+    defer infile.Close()
+
+    // Decode will figure out what type of image is in the file on its own.
+    // We just have to be sure all the image packages we want are imported.
+    src, _, err := image.Decode(infile)
+    if err != nil {
+        // replace this with real error handling
+        panic(err)
+    }
+
+    return src
+}
+
+func GetImageBounds(filename string) (int, int) {
+    src := DecodeImage(filename)
+
+    bounds := src.Bounds()
+    w, h := bounds.Max.X, bounds.Max.Y
+
+    return w, h
+}
+
+
+func MergeImages(files []File, maxWidth, maxHeight, itemsPerRow int) image.Image {
+    var x, y int
+    var curr int
+
+    img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{maxWidth, maxHeight}})
+    maxHeight = 0
+
+    for _, f := range files {
+        if strings.HasPrefix(f.Name, ".") {
+            continue
+        }
+
+        src := DecodeImage("images/" + f.Name)
+        b := src.Bounds()
+
+        if b.Max.Y > maxHeight {
+            maxHeight = b.Max.Y
+        }
+
+        var a = image.Point{x, y}
+
+        draw.Draw(
+            img,
+            image.Rectangle{a, a.Add(image.Point{b.Max.X, b.Max.Y})},
+            src,
+            image.ZP,
+            draw.Src)
+
+        x += b.Max.X
+        curr++
+
+        if curr % itemsPerRow == 0 {
+            x = 0
+            y += maxHeight
+            maxHeight = 0
+        }
+    }
+
+    return img
+}
+
+func CalculateMaxWidthAndHeight(files []File, itemsPerRow int) (int, int) {
+    var maxWidth, maxHeight int
+    var curr, currWidth, currHeight int
+
+    for _, f := range files {
+        currWidth += f.W
+
+        if currWidth > maxWidth {
+            maxWidth = currWidth
+        }
+
+        if curr % itemsPerRow == 0 {
+            currWidth = 0
+            currHeight += f.H
+        }
+
+        if currHeight > maxHeight {
+            maxHeight = currHeight
+        }
+
+        curr++
+    }
+
+    return maxWidth, maxHeight
+}

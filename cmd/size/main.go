65108df37ffcb739c482c6fdbfe98f7cd6100aba
Juan van den Anker <juan@vdanker.net>
2018-06-19 09:46:33 +1200

    draft version of resize code

--- cmd/size/main.go
+++ cmd/size/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+    "os"
+    "fmt"
+    "image/png"
+    "encoding/json"
+
+    "github.com/jvdanker/go-test/util"
+    "github.com/nfnt/resize"
+)
+
+type ProcessedImage struct {
+    Original util.File
+    Processed util.File
+}
 
 func main() {
-    fmt.Println("size")
+    files := util.GetImages("./images")
+
+    fmt.Printf("Images to process : %d\n", len(files))
+
+    result := []ProcessedImage{}
+
+    for _, f := range files {
+        image := util.DecodeImage("./images/" + f.Name);
+        image2 := resize.Thumbnail(400, 300, image, resize.NearestNeighbor)
+
+        newName := "./output/" + f.Name + "_400x300.png"
+        outfile, err := os.Create(newName)
+        if err != nil {
+            panic(err)
+        }
+        defer outfile.Close()
+        png.Encode(outfile, image2)
+
+        w, h := util.GetImageBounds(newName)
+        newFile := util.File{ Name: newName, W: w, H: h }
+        pi := ProcessedImage{ f, newFile }
+
+        result = append(result, pi)
+    }
+
+    b, err := json.MarshalIndent(result, "", "  ")
+    if err != nil {
+        fmt.Println("error:", err)
+    }
+
+    outfile, err := os.Create("./output/manifest.json")
+    if err != nil {
+        panic(err)
+    }
+    defer outfile.Close()
+    outfile.Write(b)
 }

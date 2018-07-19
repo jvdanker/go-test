c5c6a81ad60935f6c886ae0797f1f05b14b69525
Juan van den Anker <juan@vdanker.net>
2018-07-19 15:59:53 +1200

    refactoring

--- cmd/pipelines/main.go
+++ cmd/pipelines/main.go
@@ -9,22 +9,10 @@ func main() {
 	fmt.Println("Pipelines Test")
 
 	c := numsToChan(1, 2, 3, 4, 5)
-
-	cs := fanout(c, 2)
-
-	for n := range merge(cs...) {
-		fmt.Println(n) // 4 then 9, or 9 then 4
+	cs := createWorkers(c, 1)
+	for n := range mergeWorkers(cs...) {
+		fmt.Printf("merged output n=%v\n", n)
 	}
-}
-
-func fanout(c <-chan int, count int) []<-chan int {
-	result := make([]<-chan int, 0)
-
-	for i := 0; i < count; i++ {
-		result = append(result, readChan(c))
-	}
-
-	return result
 }
 
 func numsToChan(nums ...int) <-chan int {
@@ -32,6 +20,7 @@ func numsToChan(nums ...int) <-chan int {
 
 	go func() {
 		for _, j := range nums {
+			fmt.Printf("output to channel, n=%v\n", j)
 			out <- j
 		}
 
@@ -41,20 +30,32 @@ func numsToChan(nums ...int) <-chan int {
 	return out
 }
 
-func readChan(in <-chan int) <-chan int {
+func createWorkers(c <-chan int, count int) []<-chan int {
+	result := make([]<-chan int, 0)
+
+	for i := 0; i < count; i++ {
+		fmt.Printf("create worker %v\n", i)
+		result = append(result, channelWorker(c, i))
+	}
+
+	return result
+}
+
+func channelWorker(in <-chan int, id int) <-chan int {
 	out := make(chan int)
 
-	go func() {
+	go func(id int) {
 		for n := range in {
+			fmt.Printf("worker=%v, n=%v\n", id, n)
 			out <- n
 		}
 		close(out)
-	}()
+	}(id)
 
 	return out
 }
 
-func merge(cs ...<-chan int) <-chan int {
+func mergeWorkers(cs ...<-chan int) <-chan int {
 	var wg sync.WaitGroup
 	out := make(chan int)
 

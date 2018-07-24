5594d39f19095b8c292f2cd80c8bf697ccf02a71
Juan van den Anker <juan@vdanker.net>
2018-07-24 16:01:55 +1200

    timings

--- cmd/pipelines/main.go
+++ cmd/pipelines/main.go
@@ -2,29 +2,61 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 func main() {
-	fmt.Println("Pipelines Test")
+	//fmt.Println("Pipelines Test")
 
-	c := numsToChan(1, 2, 3, 4, 5)
-	cs := createWorkers(c, 1)
-	for n := range mergeWorkers(cs...) {
-		fmt.Printf("merged output n=%v\n", n)
+	var data []int
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+	for i := 0; i < 20; i++ {
+		data = append(data, r.Int())
 	}
+
+	c := numsToChan(data...)
+	cs := createWorkers(c, 2)
+	merged := mergeWorkers(cs...)
+	for m := range merged {
+		//fmt.Printf("merged output n=%v\n", n)
+		a := m
+		m = a
+	}
+
+	fmt.Printf("\nmin=%v, max=%v\n", min, max)
+}
+
+var min int64 = math.MaxInt64
+var max int64
+
+func timings(f string, start int64) {
+	end := time.Now().UnixNano()
+	if start < min {
+		min = start
+	}
+	if end > max {
+		max = end
+	}
+
+	fmt.Printf("%v, %v, %v\n", f, start, end)
 }
 
 func numsToChan(nums ...int) <-chan int {
 	out := make(chan int)
 
 	go func() {
+		start := time.Now().UnixNano()
 		for _, j := range nums {
-			fmt.Printf("output to channel, n=%v\n", j)
+			//fmt.Printf("output to channel, n=%v\n", j)
 			out <- j
 		}
 
 		close(out)
+		timings("numsToChan", start)
 	}()
 
 	return out
@@ -34,7 +66,7 @@ func createWorkers(c <-chan int, count int) []<-chan int {
 	result := make([]<-chan int, 0)
 
 	for i := 0; i < count; i++ {
-		fmt.Printf("create worker %v\n", i)
+		//fmt.Printf("create worker %v\n", i)
 		result = append(result, channelWorker(c, i))
 	}
 
@@ -45,11 +77,16 @@ func channelWorker(in <-chan int, id int) <-chan int {
 	out := make(chan int)
 
 	go func(id int) {
+		start := time.Now().UnixNano()
 		for n := range in {
-			fmt.Printf("worker=%v, n=%v\n", id, n)
+			start2 := time.Now().UnixNano()
+			time.Sleep(1 * time.Millisecond)
+			//fmt.Printf("worker=%v, n=%v\n", id, n)
 			out <- n
+			timings(fmt.Sprintf("channelWorker--(%v)", id), start2)
 		}
 		close(out)
+		timings(fmt.Sprintf("channelWorker(%v)", id), start)
 	}(id)
 
 	return out
@@ -59,16 +96,18 @@ func mergeWorkers(cs ...<-chan int) <-chan int {
 	var wg sync.WaitGroup
 	out := make(chan int)
 
-	output := func(c <-chan int) {
+	digest := func(c <-chan int) {
+		start := time.Now().UnixNano()
 		for n := range c {
 			out <- n
 		}
 		wg.Done()
+		timings("mergeWorkers", start)
 	}
 
 	wg.Add(len(cs))
 	for _, c := range cs {
-		go output(c)
+		go digest(c)
 	}
 
 	go func() {
